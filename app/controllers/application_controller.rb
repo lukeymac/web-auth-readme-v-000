62807296016d313399f693f3980e5b8da0d19fb4
lukeymac <lukey.mac@outlook.com>
2017-07-21 15:27:55 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # arbitrary change for 'learn submit'. read documentation, didn't code it here
+  
 end
